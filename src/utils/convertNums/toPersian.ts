66cfc384a7e3988f0cad03ee13a5e5da4c9a0f92
Ashkan <ashkan.ahrabi@podro.com>
2021-12-18 01:00:20 +0330

    [WIP] add try catch

--- src/utils/convertNums/toPersian.ts
+++ src/utils/convertNums/toPersian.ts
@@ -1,4 +1,10 @@
 export const toPersian = (value: string): string => {
     const id: Array<string> = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-    return value.replace(/[0-9]/g, (x: string) => id[+x]);
+    //TODO: handle exceptions
+    try {
+        return value.replace(/[0-9]/g, (x: string) => id[+x]);
+    }
+    catch (e) {
+        throw Error;
+    }
 };
