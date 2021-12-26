621840c2da11bbd01500761ee9ae7c4be9927cc0
Ashkan <ashkan.ahrabi@podro.com>
2021-12-26 23:45:44 +0330

    [fix] missing export statements

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { ConvertNums } from './utils/convertNums';
-import { CurrencyFormatter } from './utils/currencyFormatter';
-import { Validations } from './validations';
-
-// console.log(ConvertNums.toPersian("1230"));
-// console.log(ConvertNums.toEnglish("۱۲۳"));
-// console.log(CurrencyFormatter.format("9123333"));
-// console.log(Validations.isNationalCode("۰۰۱۷۵۳۲۰۰۰"));
-// console.log(Validations.isMobile("09129743764"));
-
-export {};
+export { ConvertNums } from './utils/convertNums';
+export { CurrencyFormatter } from './utils/currencyFormatter';
+export { Validations } from './validations';
