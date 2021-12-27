7a5b3fa515907e5d1ddb903acf06f46bbc532bbd
Ashkan <ashkan.ahrabi@podro.com>
2021-12-27 20:34:36 +0330

    [fix] strings with no digits replacement with empty strings

--- src/utils/currencyFormatter/currencyFormatter.ts
+++ src/utils/currencyFormatter/currencyFormatter.ts
@@ -1,5 +1,5 @@
 import { ConvertNums } from '../convertNums';
 export const format = (value: string): string => {
   value = ConvertNums.toEnglish(value);
-  return ConvertNums.toPersian(value.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  return ConvertNums.toPersian(value.replace(/\B(?=(\d{3})+(?!\d))/g, ','));
 };
