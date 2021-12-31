eeb6262fe2294dc74a061b34dd18847b45998293
Ashkan <ashkan.ahrabi@podro.com>
2021-12-31 15:03:48 +0330

    [fix] validation bug with Persian numbers

--- src/validations/isMobile/isMobile.ts
+++ src/validations/isMobile/isMobile.ts
@@ -1,5 +1,6 @@
-// TODO: fix parameters with Persian numbers
-// TODO: tests
+import { ConvertNums } from '../../utils/convertNums';
+
 export const isMobile = (value: string): boolean => {
+  value = ConvertNums.toEnglish(value);
   return /^(09)[0-9]{9}$/.test(value);
 };
