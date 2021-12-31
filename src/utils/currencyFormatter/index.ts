8d6c0a90f7d0b66daaa2a026b00263218c60f732
Ashkan <ashkan.ahrabi@podro.com>
2021-12-31 15:04:54 +0330

    write docs

--- src/utils/currencyFormatter/index.ts
+++ src/utils/currencyFormatter/index.ts
@@ -1,9 +1,30 @@
-import { format } from './currencyFormatter';
+import { format as formatter } from './currencyFormatter';
 
-interface CurrencyFormatter {
-  format: (value: string) => string;
+/**
+ * Formats currencies with comma on each 3rd digit
+ *
+ * *Basic import example:*
+ * ```
+ * import { CurrencyFormatter } from "palidation.js";
+ * ```
+ */
+export namespace CurrencyFormatter{
+
+  /**
+   * this method will find digits through a sting and returns a currency-formatted string with Persian digits
+   *
+   * @param value the parameter can contain any string values
+   * @return the original string with its digits currency-formatted in Persian
+   *
+   * @example
+   * ```
+   * import { CurrencyFormatter } from "palidation.js";
+   *
+   * let input = 'قیمت: 5000 تومان'; //Any string (containing English digits)
+   *
+   * console.log(CurrencyFormatter.format(input));
+   * // Output: 'قیمت: ۵,۰۰۰ تومان'
+   * ```
+   */
+  export const format = formatter;
 }
-
-export const CurrencyFormatter: CurrencyFormatter = {
-  format,
-};
